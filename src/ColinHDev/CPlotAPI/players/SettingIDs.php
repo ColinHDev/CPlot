ea5078623eb16e6c11729e01bf0b7505fa6bb04c
ColinHDev <colinheidfeld@gmail.com>
2021-08-17 14:15:50 +0200

    renamed setting IDs of flags category for possible rewrite of settings system

--- src/ColinHDev/CPlotAPI/players/SettingIDs.php
+++ src/ColinHDev/CPlotAPI/players/SettingIDs.php
@@ -18,74 +18,74 @@ interface SettingIDs {
     public const SETTING_INFORM_PLOT_RATE_ADD = "inform_add_plot_rate";
 
     public const SETTING_WARN_FLAG_TITLE = "warn_flag_title";
-    public const SETTING_WARN_FLAG_TITLE_CHANGE = "warn_flag_title_change";
-    public const SETTING_TELEPORT_FLAG_TITLE_CHANGE = "teleport_flag_title_change";
+    public const SETTING_WARN_CHANGE_FLAG_TITLE = "warn_change_flag_title";
+    public const SETTING_TELEPORT_CHANGE_FLAG_TITLE = "teleport_change_flag_title";
 
     public const SETTING_WARN_FLAG_PLOT_ENTER = "warn_flag_plot_enter";
-    public const SETTING_WARN_FLAG_PLOT_ENTER_CHANGE = "warn_flag_plot_enter_change";
-    public const SETTING_TELEPORT_FLAG_PLOT_ENTER_CHANGE = "teleport_flag_plot_enter_change";
+    public const SETTING_WARN_CHANGE_FLAG_PLOT_ENTER = "warn_change_flag_plot_enter";
+    public const SETTING_TELEPORT_CHANGE_FLAG_PLOT_ENTER = "teleport_change_flag_plot_enter";
 
     public const SETTING_WARN_FLAG_PLOT_LEAVE = "warn_flag_plot_leave";
-    public const SETTING_WARN_FLAG_PLOT_LEAVE_CHANGE = "warn_flag_plot_leave_change";
-    public const SETTING_TELEPORT_FLAG_PLOT_LEAVE_CHANGE = "teleport_flag_plot_leave_change";
+    public const SETTING_WARN_CHANGE_FLAG_PLOT_LEAVE = "warn_change_flag_plot_leave";
+    public const SETTING_TELEPORT_CHANGE_FLAG_PLOT_LEAVE = "teleport_change_flag_plot_leave";
 
     public const SETTING_WARN_FLAG_MESSAGE = "warn_flag_message";
-    public const SETTING_WARN_FLAG_MESSAGE_CHANGE = "warn_flag_message_change";
-    public const SETTING_TELEPORT_FLAG_MESSAGE_CHANGE = "teleport_flag_message_change";
+    public const SETTING_WARN_CHANGE_FLAG_MESSAGE = "warn_change_flag_message";
+    public const SETTING_TELEPORT_CHANGE_FLAG_MESSAGE = "teleport_change_flag_message";
 
     public const SETTING_WARN_FLAG_SPAWN = "warn_flag_spawn";
-    public const SETTING_WARN_FLAG_SPAWN_CHANGE = "warn_flag_spawn_change";
-    public const SETTING_TELEPORT_FLAG_SPAWN_CHANGE = "teleport_flag_spawn_change";
+    public const SETTING_WARN_CHANGE_FLAG_SPAWN = "warn_change_flag_spawn";
+    public const SETTING_TELEPORT_CHANGE_FLAG_SPAWN = "teleport_change_flag_spawn";
 
     public const SETTING_WARN_FLAG_ITEM_DROP = "warn_flag_item_drop";
-    public const SETTING_WARN_FLAG_ITEM_DROP_CHANGE = "warn_flag_item_drop_change";
-    public const SETTING_TELEPORT_FLAG_ITEM_DROP_CHANGE = "teleport_flag_item_drop_change";
+    public const SETTING_WARN_CHANGE_FLAG_ITEM_DROP = "warn_change_flag_item_drop";
+    public const SETTING_TELEPORT_CHANGE_FLAG_ITEM_DROP = "teleport_change_flag_item_drop";
 
     public const SETTING_WARN_FLAG_ITEM_PICKUP = "warn_flag_item_pickup";
-    public const SETTING_WARN_FLAG_ITEM_PICKUP_CHANGE = "warn_flag_item_pickup_change";
-    public const SETTING_TELEPORT_FLAG_ITEM_PICKUP_CHANGE = "teleport_flag_item_pickup_change";
+    public const SETTING_WARN_CHANGE_FLAG_ITEM_PICKUP = "warn_change_flag_item_pickup";
+    public const SETTING_TELEPORT_CHANGE_FLAG_ITEM_PICKUP = "teleport_change_flag_item_pickup";
 
     public const SETTING_WARN_FLAG_PVP = "warn_flag_pvp";
-    public const SETTING_WARN_FLAG_PVP_CHANGE = "warn_flag_pvp_change";
-    public const SETTING_TELEPORT_FLAG_PVP_CHANGE = "teleport_flag_pvp_change";
+    public const SETTING_WARN_CHANGE_FLAG_PVP = "warn_change_flag_pvp";
+    public const SETTING_TELEPORT_CHANGE_FLAG_PVP = "teleport_change_flag_pvp";
 
     public const SETTING_WARN_FLAG_EXPLOSION = "warn_flag_explosion";
-    public const SETTING_WARN_FLAG_EXPLOSION_CHANGE = "warn_flag_explosion_change";
-    public const SETTING_TELEPORT_FLAG_EXPLOSION_CHANGE = "teleport_flag_explosion_change";
+    public const SETTING_WARN_CHANGE_FLAG_EXPLOSION = "warn_change_flag_explosion";
+    public const SETTING_TELEPORT_CHANGE_FLAG_EXPLOSION = "teleport_change_flag_explosion";
 
     public const SETTING_WARN_FLAG_BURNING = "warn_flag_burning";
-    public const SETTING_WARN_FLAG_BURNING_CHANGE = "warn_flag_burning_change";
-    public const SETTING_TELEPORT_FLAG_BURNING_CHANGE = "teleport_flag_burning_change";
+    public const SETTING_WARN_CHANGE_FLAG_BURNING = "warn_change_flag_burning";
+    public const SETTING_TELEPORT_CHANGE_FLAG_BURNING = "teleport_change_flag_burning";
 
     public const SETTING_WARN_FLAG_FLOWING = "warn_flag_flowing";
-    public const SETTING_WARN_FLAG_FLOWING_CHANGE = "warn_flag_flowing_change";
-    public const SETTING_TELEPORT_FLAG_FLOWING_CHANGE = "teleport_flag_flowing_change";
+    public const SETTING_WARN_CHANGE_FLAG_FLOWING = "warn_change_flag_flowing";
+    public const SETTING_TELEPORT_CHANGE_FLAG_FLOWING = "teleport_change_flag_flowing";
 
     public const SETTING_WARN_FLAG_GROWING = "warn_flag_growing";
-    public const SETTING_WARN_FLAG_GROWING_CHANGE = "warn_flag_growing_change";
-    public const SETTING_TELEPORT_FLAG_GROWING_CHANGE = "teleport_flag_growing_change";
+    public const SETTING_WARN_CHANGE_FLAG_GROWING = "warn_change_flag_growing";
+    public const SETTING_TELEPORT_CHANGE_FLAG_GROWING = "teleport_change_flag_growing";
 
     public const SETTING_WARN_FLAG_PLAYER_INTERACT = "warn_flag_player_interact";
-    public const SETTING_WARN_FLAG_PLAYER_INTERACT_CHANGE = "warn_flag_player_interact_change";
-    public const SETTING_TELEPORT_FLAG_PLAYER_INTERACT_CHANGE = "teleport_flag_player_interact_change";
+    public const SETTING_WARN_CHANGE_FLAG_PLAYER_INTERACT = "warn_change_flag_player_interact";
+    public const SETTING_TELEPORT_CHANGE_FLAG_PLAYER_INTERACT = "teleport_change_flag_player_interact";
 
     public const SETTING_WARN_FLAG_SERVER_PLOT = "warn_flag_server_plot";
-    public const SETTING_WARN_FLAG_SERVER_PLOT_CHANGE = "warn_flag_server_plot_change";
-    public const SETTING_TELEPORT_FLAG_SERVER_PLOT_CHANGE = "teleport_flag_server_plot_change";
+    public const SETTING_WARN_CHANGE_FLAG_SERVER_PLOT = "warn_change_flag_server_plot";
+    public const SETTING_TELEPORT_CHANGE_FLAG_SERVER_PLOT = "teleport_change_flag_server_plot";
 
     public const SETTING_WARN_FLAG_CHECK_INACTIVE = "warn_flag_check_inactive";
-    public const SETTING_WARN_FLAG_CHECK_INACTIVE_CHANGE = "warn_flag_check_inactive_change";
-    public const SETTING_TELEPORT_FLAG_CHECK_INACTIVE_CHANGE = "teleport_flag_check_inactive_change";
+    public const SETTING_WARN_CHANGE_FLAG_CHECK_INACTIVE = "warn_change_flag_check_inactive";
+    public const SETTING_TELEPORT_CHANGE_FLAG_CHECK_INACTIVE = "teleport_change_flag_check_inactive";
 
     public const SETTING_WARN_FLAG_PLACE = "warn_flag_place";
-    public const SETTING_WARN_FLAG_PLACE_CHANGE = "warn_flag_place_change";
-    public const SETTING_TELEPORT_FLAG_PLACE_CHANGE = "teleport_flag_place_change";
+    public const SETTING_WARN_CHANGE_FLAG_PLACE = "warn_change_flag_place";
+    public const SETTING_TELEPORT_CHANGE_FLAG_PLACE = "teleport_change_flag_place";
 
     public const SETTING_WARN_FLAG_BREAK = "warn_flag_break";
-    public const SETTING_WARN_FLAG_BREAK_CHANGE = "warn_flag_break_change";
-    public const SETTING_TELEPORT_FLAG_BREAK_CHANGE = "teleport_flag_break_change";
+    public const SETTING_WARN_CHANGE_FLAG_BREAK = "warn_change_flag_break";
+    public const SETTING_TELEPORT_CHANGE_FLAG_BREAK = "teleport_change_flag_break";
 
     public const SETTING_WARN_FLAG_USE = "warn_flag_use";
-    public const SETTING_WARN_FLAG_USE_CHANGE = "warn_flag_use_change";
-    public const SETTING_TELEPORT_FLAG_USE_CHANGE = "teleport_flag_use_change";
+    public const SETTING_WARN_CHANGE_FLAG_USE = "warn_change_flag_use";
+    public const SETTING_TELEPORT_CHANGE_FLAG_USE = "teleport_change_flag_use";
 }
