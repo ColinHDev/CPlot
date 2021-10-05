b59cfce6b1b5a3bd07b5a37df95f27bc51c86846
ColinHDev <colinheidfeld@gmail.com>
2021-10-05 23:46:57 +0200

    PlotPlayer class now holds the player's player data instead of just its UUID

--- src/ColinHDev/CPlotAPI/plots/PlotPlayer.php
+++ src/ColinHDev/CPlotAPI/plots/PlotPlayer.php
@@ -2,24 +2,26 @@
 
 namespace ColinHDev\CPlotAPI\plots;
 
+use ColinHDev\CPlotAPI\players\PlayerData;
+
 class PlotPlayer {
 
     public const STATE_TRUSTED = "state_trusted";
     public const STATE_HELPER = "state_helper";
     public const STATE_DENIED = "state_denied";
 
-    private string $playerUUID;
+    private PlayerData $playerData;
     private string $state;
     private int $addTime;
 
-    public function __construct(string $playerUUID, string $state, ?int $addTime = null) {
-        $this->playerUUID = $playerUUID;
+    public function __construct(PlayerData $playerData, string $state, ?int $addTime = null) {
+        $this->playerData = $playerData;
         $this->state = $state;
         $this->addTime = ($addTime !== null) ? $addTime : (int) (round(microtime(true) * 1000));
     }
 
-    public function getPlayerUUID() : string {
-        return $this->playerUUID;
+    public function getPlayerData() : PlayerData {
+        return $this->playerData;
     }
 
     public function getState() : string {
@@ -32,14 +34,14 @@ class PlotPlayer {
 
     public function __serialize() : array {
         return [
-            "playerUUID" => $this->playerUUID,
+            "playerData" => serialize($this->playerData),
             "state" => $this->state,
             "addTime" => $this->addTime
         ];
     }
 
     public function __unserialize(array $data) : void {
-        $this->playerUUID = $data["playerUUID"];
+        $this->playerData = unserialize($data["playerData"]);
         $this->state = $data["state"];
         $this->addTime = $data["addTime"];
     }
