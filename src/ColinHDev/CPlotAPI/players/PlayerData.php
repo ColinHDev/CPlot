17b30e62ed3c944025644cb4aa92f5ea8c8ef38e
ColinHDev <colinheidfeld@gmail.com>
2021-10-07 00:24:50 +0200

    moved Plot::getOwnerLastPlayed() to PlayerData::getLastPlayed()

--- src/ColinHDev/CPlotAPI/players/PlayerData.php
+++ src/ColinHDev/CPlotAPI/players/PlayerData.php
@@ -4,8 +4,12 @@ namespace ColinHDev\CPlotAPI\players;
 
 use ColinHDev\CPlot\CPlot;
 use ColinHDev\CPlot\provider\cache\Cacheable;
+use ColinHDev\CPlot\ResourceManager;
 use ColinHDev\CPlotAPI\players\settings\BaseSetting;
 use ColinHDev\CPlotAPI\players\settings\SettingManager;
+use pocketmine\player\OfflinePlayer;
+use pocketmine\Server;
+use Ramsey\Uuid\Uuid;
 
 class PlayerData implements Cacheable {
 
@@ -30,8 +34,44 @@ class PlayerData implements Cacheable {
         return $this->playerName;
     }
 
-    public function getLastPlayed() : int {
-        return $this->lastPlayed;
+    /**
+     * @return int | null
+     * returns int as the last played time in seconds
+     * returns null if the result couldn't be found
+     */
+    public function getLastPlayed() : ?int {
+        // player is online and therefore not inactive
+        $player = Server::getInstance()->getPlayerByRawUUID(Uuid::fromString($this->playerUUID));
+        if ($player !== null) {
+            return (int) (microtime(true) * 1000);
+        }
+
+        // check if the last time the player played should be fetched from the offline data file or the database
+        switch (ResourceManager::getInstance()->getConfig()->get("lastPlayed.origin", "database")) {
+            case "offline_data":
+                // if the player isn't an instance of OfflinePlayer it is one of Player and therefore online on the server
+                $offlinePlayer = Server::getInstance()->getOfflinePlayer($this->playerName);
+                if (!$offlinePlayer instanceof OfflinePlayer) {
+                    return (int) (microtime(true) * 1000);
+                }
+
+                // check if the player's offline player data even exists
+                // if not we try to fetch the last time the player played from the database
+                // this could be null if the server admin deleted the player's offline data file
+                $lastPlayed = $offlinePlayer->getLastPlayed();
+                if ($lastPlayed !== null) {
+                    break;
+                }
+
+            default:
+            case "database":
+                $lastPlayed = $this->lastPlayed;
+                break;
+        }
+
+        // the last played time is saved in milliseconds therefore we devide by 1000 and cast it to an integer
+        // the float gets rounded up in favor of the player
+        return (int) ceil($lastPlayed / 1000);
     }
 
 
