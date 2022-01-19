215285ab505d7dfc2d5cee937a4a3452f8c95652
ColinHDev <colinheidfeld@gmail.com>
2022-01-19 01:49:27 +0100

    EntityMovementTask: Removed the world ID as key from the "last position" array of the entities, since entity IDs are already unique, so we don't need to differentiate between the worlds.

--- src/ColinHDev/CPlot/tasks/EntityMovementTask.php
+++ src/ColinHDev/CPlot/tasks/EntityMovementTask.php
@@ -18,7 +18,7 @@ use SOFe\AwaitGenerator\Await;
 class EntityMovementTask extends Task {
 
     private WorldManager $worldManager;
-    /** @var Vector3[][] */
+    /** @var Vector3[] */
     private array $lastVector = [];
 
     public function __construct() {
@@ -33,11 +33,6 @@ class EntityMovementTask extends Task {
                     $worldSettings = yield DataProvider::getInstance()->awaitWorld($world->getFolderName());
                     if (!$worldSettings instanceof WorldSettings) return;
 
-                    $worldId = $world->getId();
-                    if (!isset($this->lastVector[$worldId])) {
-                        $this->lastVector[$worldId] = [];
-                    }
-
                     foreach ($world->updateEntities as $entity) {
                         if ($entity instanceof Human || $entity instanceof ItemEntity) {
                             continue;
@@ -45,22 +40,22 @@ class EntityMovementTask extends Task {
 
                         $entityId = $entity->getId();
                         if ($entity->isClosed()) {
-                            unset($this->lastVector[$worldId][$entityId]);
+                            unset($this->lastVector[$entityId]);
                             continue;
                         }
 
                         if (!$entity->hasMovementUpdate()) continue;
 
                         $position = $entity->getPosition();
-                        if (!isset($this->lastVector[$worldId][$entityId])) {
-                            $this->lastVector[$worldId][$entityId] = $position->asVector3();
+                        if (!isset($this->lastVector[$entityId])) {
+                            $this->lastVector[$entityId] = $position->asVector3();
                             continue;
                         }
 
-                        $lastPosition = Position::fromObject($this->lastVector[$worldId][$entityId], $world);
+                        $lastPosition = Position::fromObject($this->lastVector[$entityId], $world);
                         if ($lastPosition->equals($position)) continue;
 
-                        $this->lastVector[$worldId][$entityId] = $position->asVector3();
+                        $this->lastVector[$entityId] = $position->asVector3();
                         $lastBasePlot = yield BasePlot::fromPosition($lastPosition);
                         $basePlot = yield BasePlot::fromPosition($position);
                         if ($lastBasePlot !== null && $basePlot !== null && (yield $lastBasePlot->isSame($basePlot))) continue;
