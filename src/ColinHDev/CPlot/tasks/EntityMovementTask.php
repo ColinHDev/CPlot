5bf506e5773ca68121a9a9446108a83f70034f6e
ColinHDev <colinheidfeld@gmail.com>
2022-01-20 01:06:42 +0100

    EntityMovementTask: Made it using the cache methode

--- src/ColinHDev/CPlot/tasks/EntityMovementTask.php
+++ src/ColinHDev/CPlot/tasks/EntityMovementTask.php
@@ -8,12 +8,10 @@ use ColinHDev\CPlotAPI\plots\Plot;
 use ColinHDev\CPlotAPI\worlds\WorldSettings;
 use pocketmine\entity\Human;
 use pocketmine\entity\object\ItemEntity;
-use pocketmine\math\Vector3;
 use pocketmine\scheduler\Task;
 use pocketmine\Server;
 use pocketmine\world\Position;
 use pocketmine\world\WorldManager;
-use SOFe\AwaitGenerator\Await;
 
 class EntityMovementTask extends Task {
 
@@ -27,57 +25,63 @@ class EntityMovementTask extends Task {
 
     public function onRun() : void {
         foreach ($this->worldManager->getWorlds() as $world) {
+            $worldName = $world->getFolderName();
+            $worldSettings = DataProvider::getInstance()->loadWorldIntoCache($worldName);
+            if (!$worldSettings instanceof WorldSettings) {
+                return;
+            }
 
-            Await::f2c(
-                function () use ($world) : \Generator {
-                    $worldSettings = yield DataProvider::getInstance()->awaitWorld($world->getFolderName());
-                    if (!$worldSettings instanceof WorldSettings) return;
-
-                    foreach ($world->updateEntities as $entity) {
-                        if ($entity instanceof Human || $entity instanceof ItemEntity) {
-                            continue;
-                        }
-
-                        $entityId = $entity->getId();
-                        if ($entity->isClosed()) {
-                            unset($this->lastPositions[$entityId]);
-                            continue;
-                        }
-
-                        if (!$entity->hasMovementUpdate()) continue;
-
-                        if (!isset($this->lastPositions[$entityId])) {
-                            $this->lastPositions[$entityId] = $entity->getPosition();
-                            continue;
-                        }
-
-                        $position = $entity->getPosition();
-                        $lastPosition = $this->lastPositions[$entityId];
-                        if (
-                            // Only if the world did not change, e.g. due to a teleport, we need to check how far the entity moved.
-                            $position->world === $lastPosition->world &&
-                            // Check if the entity moved across a block and if not, we already checked that block and the entity just
-                            // moved in the borders between that one.
-                            $position->getFloorX() === $lastPosition->getFloorX() &&
-                            $position->getFloorY() === $lastPosition->getFloorY() &&
-                            $position->getFloorZ() === $lastPosition->getFloorZ()
-                        ) {
-                            return;
-                        }
-
-                        $this->lastPositions[$entityId] = $position->asVector3();
-                        $lastBasePlot = yield BasePlot::fromPosition($lastPosition);
-                        $basePlot = yield BasePlot::fromPosition($position);
-                        if ($lastBasePlot !== null && $basePlot !== null && (yield $lastBasePlot->isSame($basePlot))) continue;
-
-                        $lastPlot = (yield $lastBasePlot?->toPlot()) ?? (yield Plot::fromPosition($lastPosition));
-                        $plot = (yield $basePlot?->toPlot()) ?? (yield Plot::fromPosition($position));
-                        if ($lastPlot !== null && $plot !== null && (yield $lastPlot->isSame($plot))) continue;
-
-                        $entity->flagForDespawn();
-                    }
+            foreach ($world->updateEntities as $entity) {
+                if ($entity instanceof Human || $entity instanceof ItemEntity) {
+                    continue;
                 }
-            );
+
+                $entityId = $entity->getId();
+                if ($entity->isClosed()) {
+                    unset($this->lastPositions[$entityId]);
+                    continue;
+                }
+
+                if (!$entity->hasMovementUpdate()) {
+                    continue;
+                }
+
+                if (!isset($this->lastPositions[$entityId])) {
+                    $this->lastPositions[$entityId] = $entity->getPosition();
+                    continue;
+                }
+
+                $lastPosition = $this->lastPositions[$entityId];
+                $position = $this->lastPositions[$entityId] = $entity->getPosition();
+                if (
+                    // Only if the world did not change, e.g. due to a teleport, we need to check how far the entity moved.
+                    $position->world === $lastPosition->world &&
+                    // Check if the entity moved across a block and if not, we already checked that block and the entity just
+                    // moved in the borders between that one.
+                    $position->getFloorX() === $lastPosition->getFloorX() &&
+                    $position->getFloorY() === $lastPosition->getFloorY() &&
+                    $position->getFloorZ() === $lastPosition->getFloorZ()
+                ) {
+                    return;
+                }
+
+                $lastBasePlot = BasePlot::fromVector3($worldName, $worldSettings, $lastPosition);
+                $basePlot = BasePlot::fromVector3($worldName, $worldSettings, $position);
+                if ($lastBasePlot !== null && $basePlot !== null && $lastBasePlot->isSame($basePlot)) {
+                    continue;
+                }
+
+                $lastPlot = $lastBasePlot?->toSyncPlot() ?? Plot::loadFromPositionIntoCache($lastPosition);
+                $plot = $basePlot?->toSyncPlot() ?? Plot::loadFromPositionIntoCache($position);
+                if ($lastPlot instanceof Plot && $plot instanceof Plot && $lastPlot->isSame($plot)) {
+                    continue;
+                }
+                if ($lastPlot instanceof BasePlot || $plot instanceof BasePlot) {
+                    continue;
+                }
+
+                $entity->flagForDespawn();
+            }
         }
     }
 }
