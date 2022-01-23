002a52f722722bbbdce2247d03f090de5a83dce9
ColinHDev <colinheidfeld@gmail.com>
2022-01-23 02:11:25 +0100

    PlotCommand: Renamed loadSubcommand() method to registerSubcommand() for clearer intention

--- src/ColinHDev/CPlot/commands/PlotCommand.php
+++ src/ColinHDev/CPlot/commands/PlotCommand.php
@@ -55,33 +55,33 @@ class PlotCommand extends Command implements PluginOwned {
         $this->setPermission("cplot.command.plot");
         $this->setPermissionMessage($resourceManager->getPrefix() . $commandData["permissionMessage"]);
 
-        $this->loadSubcommand(new AddSubcommand($resourceManager->getCommandData("add"), "cplot.subcommand.add"));
-        $this->loadSubcommand(new AutoSubcommand($resourceManager->getCommandData("auto"), "cplot.subcommand.auto"));
-        $this->loadSubcommand(new BorderSubcommand($resourceManager->getCommandData("border"), "cplot.subcommand.border"));
-        $this->loadSubcommand(new ClaimSubcommand($resourceManager->getCommandData("claim"), "cplot.subcommand.claim"));
-        $this->loadSubcommand(new ClearSubcommand($resourceManager->getCommandData("clear"), "cplot.subcommand.clear"));
-        $this->loadSubcommand(new DeniedSubcommand($resourceManager->getCommandData("denied"), "cplot.subcommand.denied"));
-        $this->loadSubcommand(new DenySubcommand($resourceManager->getCommandData("deny"), "cplot.subcommand.deny"));
-        $this->loadSubcommand(new FlagSubcommand($resourceManager->getCommandData("flag"), "cplot.subcommand.flag"));
-        $this->loadSubcommand(new GenerateSubcommand($resourceManager->getCommandData("generate"), "cplot.subcommand.generate"));
-        $this->loadSubcommand(new HelpersSubcommand($resourceManager->getCommandData("helpers"), "cplot.subcommand.helpers"));
-        $this->loadSubcommand(new HelpSubcommand($resourceManager->getCommandData("help"), "cplot.subcommand.help", $this));
-        $this->loadSubcommand(new InfoSubcommand($resourceManager->getCommandData("info"), "cplot.subcommand.info"));
-        $this->loadSubcommand(new MergeSubcommand($resourceManager->getCommandData("merge"), "cplot.subcommand.merge"));
-        $this->loadSubcommand(new RemoveSubcommand($resourceManager->getCommandData("remove"), "cplot.subcommand.remove"));
-        $this->loadSubcommand(new ResetSubcommand($resourceManager->getCommandData("reset"), "cplot.subcommand.reset"));
-        $this->loadSubcommand(new SchematicSubcommand($resourceManager->getCommandData("schematic"), "cplot.subcommand.schematic"));
-        $this->loadSubcommand(new SettingSubcommand($resourceManager->getCommandData("setting"), "cplot.subcommand.setting"));
-        $this->loadSubcommand(new TrustedSubcommand($resourceManager->getCommandData("trusted"), "cplot.subcommand.trusted"));
-        $this->loadSubcommand(new TrustSubcommand($resourceManager->getCommandData("trust"), "cplot.subcommand.trust"));
-        $this->loadSubcommand(new UndenySubcommand($resourceManager->getCommandData("undeny"), "cplot.subcommand.undeny"));
-        $this->loadSubcommand(new UntrustSubcommand($resourceManager->getCommandData("untrust"), "cplot.subcommand.untrust"));
-        $this->loadSubcommand(new VisitSubcommand($resourceManager->getCommandData("visit"), "cplot.subcommand.visit"));
-        $this->loadSubcommand(new WallSubcommand($resourceManager->getCommandData("wall"), "cplot.subcommand.wall"));
-        $this->loadSubcommand(new WarpSubcommand($resourceManager->getCommandData("warp"), "cplot.subcommand.warp"));
+        $this->registerSubcommand(new AddSubcommand($resourceManager->getCommandData("add"), "cplot.subcommand.add"));
+        $this->registerSubcommand(new AutoSubcommand($resourceManager->getCommandData("auto"), "cplot.subcommand.auto"));
+        $this->registerSubcommand(new BorderSubcommand($resourceManager->getCommandData("border"), "cplot.subcommand.border"));
+        $this->registerSubcommand(new ClaimSubcommand($resourceManager->getCommandData("claim"), "cplot.subcommand.claim"));
+        $this->registerSubcommand(new ClearSubcommand($resourceManager->getCommandData("clear"), "cplot.subcommand.clear"));
+        $this->registerSubcommand(new DeniedSubcommand($resourceManager->getCommandData("denied"), "cplot.subcommand.denied"));
+        $this->registerSubcommand(new DenySubcommand($resourceManager->getCommandData("deny"), "cplot.subcommand.deny"));
+        $this->registerSubcommand(new FlagSubcommand($resourceManager->getCommandData("flag"), "cplot.subcommand.flag"));
+        $this->registerSubcommand(new GenerateSubcommand($resourceManager->getCommandData("generate"), "cplot.subcommand.generate"));
+        $this->registerSubcommand(new HelpersSubcommand($resourceManager->getCommandData("helpers"), "cplot.subcommand.helpers"));
+        $this->registerSubcommand(new HelpSubcommand($resourceManager->getCommandData("help"), "cplot.subcommand.help", $this));
+        $this->registerSubcommand(new InfoSubcommand($resourceManager->getCommandData("info"), "cplot.subcommand.info"));
+        $this->registerSubcommand(new MergeSubcommand($resourceManager->getCommandData("merge"), "cplot.subcommand.merge"));
+        $this->registerSubcommand(new RemoveSubcommand($resourceManager->getCommandData("remove"), "cplot.subcommand.remove"));
+        $this->registerSubcommand(new ResetSubcommand($resourceManager->getCommandData("reset"), "cplot.subcommand.reset"));
+        $this->registerSubcommand(new SchematicSubcommand($resourceManager->getCommandData("schematic"), "cplot.subcommand.schematic"));
+        $this->registerSubcommand(new SettingSubcommand($resourceManager->getCommandData("setting"), "cplot.subcommand.setting"));
+        $this->registerSubcommand(new TrustedSubcommand($resourceManager->getCommandData("trusted"), "cplot.subcommand.trusted"));
+        $this->registerSubcommand(new TrustSubcommand($resourceManager->getCommandData("trust"), "cplot.subcommand.trust"));
+        $this->registerSubcommand(new UndenySubcommand($resourceManager->getCommandData("undeny"), "cplot.subcommand.undeny"));
+        $this->registerSubcommand(new UntrustSubcommand($resourceManager->getCommandData("untrust"), "cplot.subcommand.untrust"));
+        $this->registerSubcommand(new VisitSubcommand($resourceManager->getCommandData("visit"), "cplot.subcommand.visit"));
+        $this->registerSubcommand(new WallSubcommand($resourceManager->getCommandData("wall"), "cplot.subcommand.wall"));
+        $this->registerSubcommand(new WarpSubcommand($resourceManager->getCommandData("warp"), "cplot.subcommand.warp"));
     }
 
-    private function loadSubcommand(Subcommand $subcommand) : void {
+    private function registerSubcommand(Subcommand $subcommand) : void {
         $this->subcommands[$subcommand->getName()] = $subcommand;
         foreach ($subcommand->getAlias() as $alias) {
             $this->subcommands[$alias] = $subcommand;
