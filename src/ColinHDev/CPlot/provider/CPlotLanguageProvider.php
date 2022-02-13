8a0cb92548c669aef19a2beee848fb483eaf965d
ColinHDev <colinheidfeld@gmail.com>
2022-02-13 23:05:51 +0100

    implemented basic LanguageProvider classes

---
+++ src/ColinHDev/CPlot/provider/CPlotLanguageProvider.php
@@ -0,0 +1,99 @@
+<?php
+
+declare(strict_types=1);
+
+namespace ColinHDev\CPlot\provider;
+
+use ColinHDev\CPlot\CPlot;
+use ColinHDev\CPlot\ResourceManager;
+use pocketmine\command\CommandSender;
+use pocketmine\lang\Language;
+use pocketmine\player\Player;
+
+/**
+ * @phpstan-type LanguageIdentifier string
+ * @phpstan-import-type MessageKey from LanguageProvider
+ * @phpstan-import-type MessageParam from LanguageProvider
+ */
+class CPlotLanguageProvider extends LanguageProvider {
+
+    /** @phpstan-var LanguageIdentifier */
+    private string $fallbackLanguage;
+    /** @phpstan-var array<LanguageIdentifier, Language> */
+    private array $languages;
+
+    public function __construct() {
+        $this->fallbackLanguage = ResourceManager::getInstance()->getConfig()->get("fallback.language", "de_DE");
+        $dir = scandir(CPlot::getInstance()->getDataFolder() . "language");
+        if ($dir !== false) {
+            foreach ($dir as $file) {
+                $fileData = pathinfo($file);
+                if (!isset($fileData["extension"]) || $fileData["extension"] !== "ini") {
+                    continue;
+                }
+                $this->languages[$fileData["filename"]] = new Language(
+                    $fileData["filename"],
+                    CPlot::getInstance()->getDataFolder() . "language",
+                    $this->fallbackLanguage
+                );
+            }
+        }
+    }
+
+    public function translateString(array|string $keys) : string {
+        return $this->buildMessage($this->languages[$this->fallbackLanguage], $keys);
+    }
+
+    public function translateForCommandSender(CommandSender $sender, array|string $keys, \Closure $onSuccess, ?\Closure $onError = null) : void {
+        if (!($sender instanceof Player)) {
+            $language = $this->languages[$this->fallbackLanguage];
+        } else {
+            $language = $this->languages[$sender->getLocale()] ?? $this->languages[$this->fallbackLanguage];
+        }
+        $onSuccess($this->buildMessage($language, $keys));
+    }
+
+    public function sendMessage(CommandSender $sender, array|string $keys, ?\Closure $onSuccess = null, ?\Closure $onError = null) : void {
+        if (!($sender instanceof Player)) {
+            $message = $this->translateString($keys);
+        } else {
+            if (!$sender->isOnline()) {
+                return;
+            }
+            $message = $this->buildMessage($this->languages[$sender->getLocale()] ?? $this->languages[$this->fallbackLanguage], $keys);
+        }
+        $sender->sendMessage($message);
+        $onSuccess(null);
+    }
+
+    public function sendTip(Player $player, array|string $keys, ?\Closure $onSuccess = null, ?\Closure $onError = null) : void {
+        if (!$player->isOnline()) {
+            return;
+        }
+        $player->sendTip(
+            $this->buildMessage($this->languages[$player->getLocale()] ?? $this->languages[$this->fallbackLanguage], $keys)
+        );
+        $onSuccess(null);
+    }
+
+    /**
+     * @phpstan-param array<int|MessageKey, MessageKey|MessageParam|array<MessageParam>>|MessageKey $keys
+     */
+    private function buildMessage(Language $language, array|string $keys) : string {
+        if (is_string($keys)) {
+            $keys = [$keys];
+        }
+        $message = "";
+        foreach ($keys as $key => $value) {
+            if (is_string($key)) {
+                if (!is_array($value)) {
+                    $value = [$value];
+                }
+                $message .= $language->translateString($key, $value);
+            } else {
+                $message .= $language->get((string) $value);
+            }
+        }
+        return $message;
+    }
+}
---
+++ src/ColinHDev/CPlot/provider/LanguageProvider.php
@@ -0,0 +1,91 @@
+<?php
+
+declare(strict_types=1);
+
+namespace ColinHDev\CPlot\provider;
+
+use pocketmine\command\CommandSender;
+use pocketmine\player\Player;
+use SOFe\AwaitGenerator\Await;
+
+/**
+ * @phpstan-type MessageKey string
+ * @phpstan-type MessageParam float|int|string|Translatable
+ */
+abstract class LanguageProvider {
+
+    /**
+     * This method is used to translate a message using the fallback language.
+     * @phpstan-param array<int|MessageKey, MessageKey|MessageParam|array<MessageParam>>|MessageKey $keys
+     */
+    abstract public function translateString(array|string $keys) : string;
+
+    /**
+     * @internal method to translate a message for the given {@see CommandSender} while also using a {@see \Generator}
+     * function which we can handle with {@see Await}.
+     * @phpstan-param array<int|MessageKey, MessageKey|MessageParam|array<MessageParam>>|MessageKey $keys
+     * @phpstan-return \Generator<int, Await::RESOLVE|Await::REJECT|Await::ONCE, \Closure(mixed=): void|\Closure(\Throwable): void|null, null>
+     */
+    final public function awaitTranslationForCommandSender(CommandSender $sender, array|string $keys) : \Generator {
+        /** @phpstan-var \Closure(mixed=): void $onSuccess */
+        $onSuccess = yield Await::RESOLVE;
+        /** @phpstan-var \Closure(\Throwable): void $onError */
+        $onError = yield Await::REJECT;
+        $this->translateForCommandSender($sender, $keys, $onSuccess, $onError);
+        return yield Await::ONCE;
+    }
+
+    /**
+     * This method is used to translate a message using the fallback language.
+     * @phpstan-param array<int|MessageKey, MessageKey|MessageParam|array<MessageParam>>|MessageKey $keys
+     * @phpstan-param null|\Closure(string): void $onSuccess
+     * @phpstan-param null|\Closure(\Throwable): void $onError
+     */
+    abstract public function translateForCommandSender(CommandSender $sender, array|string $keys, \Closure $onSuccess, ?\Closure $onError = null) : void;
+
+    /**
+     * @internal method to send a message to the given {@see CommandSender} while also using a {@see \Generator}
+     * function which we can handle with {@see Await}.
+     * @phpstan-param array<int|MessageKey, MessageKey|MessageParam|array<MessageParam>>|MessageKey $keys
+     * @phpstan-return \Generator<int, Await::RESOLVE|Await::REJECT|Await::ONCE, \Closure(mixed=): void|\Closure(\Throwable): void|null, null>
+     */
+    final public function awaitMessageSendage(CommandSender $sender, array|string $keys) : \Generator {
+        /** @phpstan-var \Closure(mixed=): void $onSuccess */
+        $onSuccess = yield Await::RESOLVE;
+        /** @phpstan-var \Closure(\Throwable): void $onError */
+        $onError = yield Await::REJECT;
+        $this->sendMessage($sender, $keys, $onSuccess, $onError);
+        return yield Await::ONCE;
+    }
+
+    /**
+     * This method is used to send a message to the given {@see CommandSender} (e.g. a player or the console).
+     * @phpstan-param array<int|MessageKey, MessageKey|MessageParam|array<MessageParam>>|MessageKey $keys
+     * @phpstan-param null|\Closure(null): void $onSuccess
+     * @phpstan-param null|\Closure(\Throwable): void $onError
+     */
+    abstract public function sendMessage(CommandSender $sender, array|string $keys, ?\Closure $onSuccess = null, ?\Closure $onError = null) : void;
+
+    /**
+     * @internal method to send a tip to the given {@see Player} while also using a {@see \Generator}
+     * function which we can handle with {@see Await}.
+     * @phpstan-param array<int|MessageKey, MessageKey|MessageParam|array<MessageParam>>|MessageKey $keys
+     * @phpstan-return \Generator<int, Await::RESOLVE|Await::REJECT|Await::ONCE, \Closure(mixed=): void|\Closure(\Throwable): void|null, null>
+     */
+    final public function awaitTipSendage(Player $player, array|string $keys) : \Generator {
+        /** @phpstan-var \Closure(mixed=): void $onSuccess */
+        $onSuccess = yield Await::RESOLVE;
+        /** @phpstan-var \Closure(\Throwable): void $onError */
+        $onError = yield Await::REJECT;
+        $this->sendTip($player, $keys, $onSuccess, $onError);
+        return yield Await::ONCE;
+    }
+
+    /**
+     * This method is used to send a tip to the given {@see Player}.
+     * @phpstan-param array<int|MessageKey, MessageKey|MessageParam|array<MessageParam>>|MessageKey $keys
+     * @phpstan-param null|\Closure(null): void $onSuccess
+     * @phpstan-param null|\Closure(\Throwable): void $onError
+     */
+    abstract public function sendTip(Player $player, array|string $keys, ?\Closure $onSuccess = null, ?\Closure $onError = null) : void;
+}
