15506fb52e2d869407e312fa6cdcbd1f92979804
ColinHDev <colinheidfeld@gmail.com>
2022-04-16 18:29:22 +0200

    DataProvider: Fixed "INSERT OR REPLACE" being used in MySQL query instead of "REPLACE"

--- resources/sql/mysql.sql
+++ resources/sql/mysql.sql
@@ -272,7 +272,7 @@ VALUES (:playerID, :ID, :value);
 -- #      :plotFloorBlock string
 -- #      :plotFillBlock string
 -- #      :plotBottomBlock string
-INSERT OR REPLACE INTO worlds (
+REPLACE INTO worlds (
     worldName,
     worldType, biomeID,
     roadSchematic, mergeRoadSchematic, plotSchematic,
