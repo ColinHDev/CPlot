ee5c48f9090b61710c1d20a131fb7c28de3d272a
ColinHDev <colinheidfeld@gmail.com>
2022-04-19 11:47:23 +0200

    BedrockEconomyProvider: Added (possibly, non-tested) finished version of API integration

--- src/ColinHDev/CPlot/provider/BedrockEconomyProvider.php
+++ src/ColinHDev/CPlot/provider/BedrockEconomyProvider.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace ColinHDev\CPlot\provider;
 
+use cooldogedev\BedrockEconomy\api\BedrockEconomyAPI;
+use cooldogedev\BedrockEconomy\BedrockEconomy;
 use pocketmine\player\Player;
 use pocketmine\Server;
 
@@ -19,14 +21,26 @@ class BedrockEconomyProvider extends EconomyProvider {
     }
 
     public function getCurrency() : string {
+        return BedrockEconomy::getInstance()->getCurrencyManager()->getSymbol();
     }
 
     public function parseMoneyToString(float $money) : string {
+        return (string) floor($money);
     }
 
     public function getMoney(Player $player, callable $onSuccess, callable $onError) : void {
+        $promise = BedrockEconomyAPI::beta()->get(
+            $player->getName()
+        );
+        $promise->onCompletion($onSuccess, $onError);
     }
 
     public function removeMoney(Player $player, float $money, callable $onSuccess, callable $onError) : void {
+        $intMoney = (int) floor($money);
+        $promise = BedrockEconomyAPI::beta()->deduct(
+            $player->getName(),
+            $intMoney
+        );
+        $promise->onCompletion($onSuccess, $onError);
     }
 }
