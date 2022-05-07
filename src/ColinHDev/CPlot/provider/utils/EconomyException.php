7e07019cef9dcda5e8eecfebbfc9b960809d488a
ColinHDev <colinheidfeld@gmail.com>
2022-05-07 23:09:45 +0200

    EconomyException: Updated and finalized error codes

--- src/ColinHDev/CPlot/provider/utils/EconomyException.php
+++ src/ColinHDev/CPlot/provider/utils/EconomyException.php
@@ -13,32 +13,28 @@ use Exception;
  */
 class EconomyException extends Exception {
 
-    // TODO: change codes to something more universally usable
-    public const SOURCE_UNDERFLOW = 1;
-    public const DESTINATION_OVERFLOW = 2;
-    public const NO_SUCH_ACCOUNT = 3;
-    public const NO_SUCH_TRANSACTION = 4;
-    public const ACCOUNT_LABEL_ALREADY_EXISTS = 5;
-    public const ACCOUNT_LABEL_DOES_NOT_EXIST = 6;
-    public const TRANSACTION_LABEL_ALREADY_EXISTS = 7;
-    public const TRANSACTION_LABEL_DOES_NOT_EXIST = 8;
-    public const EVENT_CANCELLED = 9;
+    // This error code is used if the reason why a transaction failed is unknown.
+    public const UNKNOWN = 1;
+    // This error code is used if the transaction's event within the economy plugin was cancelled.
+    public const EVENT_CANCELLED = 2;
+    // This error code is used if the player does not have an account within the economy plugin.
+    public const SOURCE_NON_EXISTENT = 3;
+    // This error code is used if the player does not have enough money to complete the transaction.
+    public const SOURCE_UNDERFLOW = 4;
 
-    public function __construct(int $code, ?Exception $previous = null) {
+    /**
+     * @phpstan-param self::* $code
+     */
+    public function __construct(int $code = self::UNKNOWN, ?Exception $previous = null) {
         parent::__construct("", $code, $previous);
     }
 
     public function getLanguageKey() : string {
         return match ($this->code) {
-            self::SOURCE_UNDERFLOW => "Source account resultant value is too low",
-            self::DESTINATION_OVERFLOW => "Destination account resultant value is too high",
-            self::NO_SUCH_ACCOUNT => "The account does not exist",
-            self::NO_SUCH_TRANSACTION => "The transaction does not exist",
-            self::ACCOUNT_LABEL_ALREADY_EXISTS => "The account already has this label",
-            self::ACCOUNT_LABEL_DOES_NOT_EXIST => "The account does not have this label",
-            self::TRANSACTION_LABEL_ALREADY_EXISTS => "The transaction already has this label",
-            self::TRANSACTION_LABEL_DOES_NOT_EXIST => "The transaction does not have this label",
-            self::EVENT_CANCELLED => "The transaction event was cancelled"
+            self::UNKNOWN => "economy.error.unknown",
+            self::EVENT_CANCELLED => "economy.error.eventCancelled",
+            self::SOURCE_NON_EXISTENT => "economy.error.sourceNonExistent",
+            self::SOURCE_UNDERFLOW => "economy.error.sourceUnderflow"
         };
     }
 }
