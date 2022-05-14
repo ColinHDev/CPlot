8f6d15fd4f2cdba8bbadc3c9b1bdcc30957bae14
ColinHDev <colinheidfeld@gmail.com>
2022-05-14 16:57:52 +0200

    PlotClearAsyncEvent: Allowed player to be null if a plot was cleared e.g. by a plugin

--- src/ColinHDev/CPlot/event/PlotClearAsyncEvent.php
+++ src/ColinHDev/CPlot/event/PlotClearAsyncEvent.php
@@ -11,26 +11,29 @@ use pocketmine\player\Player;
 use SOFe\AwaitGenerator\Await;
 
 /**
- * This event is called when a {@see Plot} is cleared by a {@see Player}.
+ * This event is called when a {@see Plot} is cleared e.g. by a {@see Player} or another plugin.
  */
 class PlotClearAsyncEvent extends PlotAsyncEvent implements Cancellable {
     use CancellableTrait;
 
-    private Player $player;
+    private ?Player $player;
 
-    public function __construct(Plot $plot, Player $player) {
+    public function __construct(Plot $plot, ?Player $player) {
         parent::__construct($plot);
         $this->player = $player;
     }
 
-    public function getPlayer() : Player {
+    /**
+     * This method returns the {@see Player} who cleared the {@see Plot} or null if it was cleared e.g. by another plugin.
+     */
+    public function getPlayer() : ?Player {
         return $this->player;
     }
 
     /**
      * @phpstan-return \Generator<mixed, Await::RESOLVE|null|Await::RESOLVE_MULTI|Await::REJECT|Await::ONCE|Await::ALL|Await::RACE|\Generator, mixed, self>
      */
-    public static function create(Plot $plot, Player $player) : \Generator {
+    public static function create(Plot $plot, ?Player $player) : \Generator {
         $event = yield from Await::promise(
             static function ($onSuccess, $onError) use ($plot, $player) : void {
                 $event = new self($plot, $player);
