65c5d008dccb7bfc30a1ccab71510f20db80b648
ColinHDev <colinheidfeld@gmail.com>
2022-05-14 12:02:03 +0200

    CPlotAsyncTask: The callback function no longer has any parameters

--- src/ColinHDev/CPlot/tasks/async/CPlotAsyncTask.php
+++ src/ColinHDev/CPlot/tasks/async/CPlotAsyncTask.php
@@ -8,39 +8,8 @@ use pocketmine\scheduler\AsyncTask;
 
 abstract class CPlotAsyncTask extends AsyncTask {
 
-    private int $startTime;
-
-    public function __construct() {
-        $this->startTime = (int) (round(microtime(true) * 1000));
-    }
-
-    protected function getElapsedTime() : int {
-        return ((int) (round(microtime(true) * 1000))) - $this->startTime;
-    }
-
-    protected function getElapsedTimeString() : string {
-        $ms = $this->getElapsedTime();
-        $min = floor($ms / 60000);
-        $ms -= $min * 60000;
-        $s = floor($ms / 1000);
-        $ms -= $s * 1000;
-        $time = "";
-        if ($min > 0) {
-            $time .= $min . "min";
-        }
-        if ($s > 0) {
-            if ($time !== "") $time .= ", ";
-            $time .= $s . "s";
-        }
-        if ($ms > 0) {
-            if ($time !== "") $time .= ", ";
-            $time .= $ms . "ms";
-        }
-        return $time;
-    }
-
     /**
-     * @phpstan-param callable(int, string, mixed): void $onSuccess
+     * @phpstan-param callable(): void $onSuccess
      * @phpstan-param null|callable(): void $onError
      */
     public function setCallback(callable $onSuccess, ?callable $onError = null) : void {
@@ -52,9 +21,9 @@ abstract class CPlotAsyncTask extends AsyncTask {
 
     public function onCompletion() : void {
         try {
-            /** @phpstan-var callable(int, string, mixed): void $callback */
+            /** @phpstan-var callable(): void $callback */
             $callback = $this->fetchLocal("onSuccess");
-            $callback($this->getElapsedTime(), $this->getElapsedTimeString(), $this->getResult());
+            $callback();
         } catch (\InvalidArgumentException) {
         }
     }
