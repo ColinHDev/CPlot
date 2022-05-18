1c2f24982c76788d85eedb6f0285f6db3181200e
ColinHDev <colinheidfeld@gmail.com>
2022-05-18 14:43:47 +0200

    PlotMergeAsyncEvent: Removed player attribute

--- src/ColinHDev/CPlot/event/PlotMergeAsyncEvent.php
+++ src/ColinHDev/CPlot/event/PlotMergeAsyncEvent.php
@@ -7,39 +7,32 @@ namespace ColinHDev\CPlot\event;
 use ColinHDev\CPlot\plots\Plot;
 use pocketmine\event\Cancellable;
 use pocketmine\event\CancellableTrait;
-use pocketmine\player\Player;
 use SOFe\AwaitGenerator\Await;
 
 /**
- * This event is called when two {@see Plot}s are merged by a {@see Player}.
+ * This event is called when two {@see Plot}s are merged.
  */
 class PlotMergeAsyncEvent extends PlotAsyncEvent implements Cancellable {
     use CancellableTrait;
 
     private Plot $plotToMerge;
-    private Player $player;
 
-    public function __construct(Plot $plot, Plot $plotToMerge, Player $player) {
+    public function __construct(Plot $plot, Plot $plotToMerge) {
         parent::__construct($plot);
         $this->plotToMerge = $plotToMerge;
-        $this->player = $player;
     }
 
     public function getPlotToMerge() : Plot {
         return $this->plotToMerge;
     }
 
-    public function getPlayer() : Player {
-        return $this->player;
-    }
-
     /**
      * @phpstan-return \Generator<mixed, Await::RESOLVE|null|Await::RESOLVE_MULTI|Await::REJECT|Await::ONCE|Await::ALL|Await::RACE|\Generator, mixed, self>
      */
-    public static function create(Plot $plot, Plot $plotToMerge, Player $player) : \Generator {
+    public static function create(Plot $plot, Plot $plotToMerge) : \Generator {
         $event = yield from Await::promise(
-            static function ($onSuccess, $onError) use ($plot, $plotToMerge, $player) : void {
-                $event = new self($plot, $plotToMerge, $player);
+            static function ($onSuccess, $onError) use ($plot, $plotToMerge) : void {
+                $event = new self($plot, $plotToMerge);
                 $event->setCallback($onSuccess);
                 $event->call();
             }
