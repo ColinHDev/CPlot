6033bd73308563bb6fa15731cb3c041fc1601d2e
ColinHDev <colinheidfeld@gmail.com>
2022-07-10 00:11:34 +0200

    CPlotAPI: Added getOrLoadPlotAtPosition() method

--- src/ColinHDev/CPlot/CPlotAPI.php
+++ src/ColinHDev/CPlot/CPlotAPI.php
@@ -10,9 +10,19 @@ use ColinHDev\CPlot\plots\Plot;
 use ColinHDev\CPlot\provider\DataProvider;
 use ColinHDev\CPlot\worlds\NonWorldSettings;
 use ColinHDev\CPlot\worlds\WorldSettings;
+use pocketmine\math\Facing;
+use pocketmine\math\Vector3;
 use pocketmine\plugin\ApiVersion;
+use pocketmine\utils\AssumptionFailedError;
 use pocketmine\utils\VersionString;
+use pocketmine\world\Position;
 use pocketmine\world\World;
+use RuntimeException;
+use function abs;
+use function ceil;
+use function count;
+use function floor;
+use function is_array;
 
 final class CPlotAPI {
 
@@ -178,5 +188,310 @@ final class CPlotAPI {
             },
             $onError
         );
+    }
+
+    /**
+     * Get the {@see Plot} at the given {@see Position}.
+     *
+     * @param Position $position The position to get the plot of
+     *
+     * If data about the plot is cached, the $onSuccess function is called immediately, while also letting the method
+     * return either the {@see Plot} or false.
+     * If no data about the plot is cached, it needs to be asychronously loaded from the database. Once this is done,
+     * the $onSuccess function is called and the result cached for the next call of this method.
+     *
+     * @param Closure|null $onSuccess The callback function to call if the data about the plot is cached or loaded
+     *                                successfully.
+     * @phpstan-param (Closure(Plot|false):void)|null $onSuccess
+     *
+     * @param Closure|null $onError The callback function to call if something went wrong during the loading of the
+     *                              data from the database.
+     * @phpstan-param (Closure():void)|(Closure(Throwable):void)|null $onError
+     *
+     * Returns the {@see Plot} at the given {@see Position}, false if there is no plot there, or null if there is no
+     * cached data about the plot, which could synchronously be gotten.
+     * @return Plot|false|null
+     *
+     * @throws RuntimeException when called outside of main thread.
+     * @throws AssumptionFailedError if given a {@see Position} with an invalid or unloaded {@see World}.
+     */
+    public function getOrLoadPlotAtPosition(Position $position, ?Closure $onSuccess = null, ?Closure $onError = null) : Plot|false|null {
+        $world = $position->getWorld();
+        $worldSettings = $this->getOrLoadWorldSettings(
+            $world,
+            function(WorldSettings|false $worldSettings) use($position, $world, $onSuccess, $onError) : void {
+                if ($worldSettings === false) {
+                    if ($onSuccess !== null) {
+                        $onSuccess(false);
+                    }
+                    return;
+                }
+                $worldName = $world->getFolderName();
+                $basePlot = $this->getBasePlotAtPoint($worldName, $worldSettings, $position);
+                if ($basePlot instanceof BasePlot) {
+                    $this->getOrLoadPlot($world, $basePlot->getX(), $basePlot->getZ(), $onSuccess, $onError);
+                    return;
+                }
+                $roadSize = $worldSettings->getRoadSize();
+                $basePlotInNorth = $this->getBasePlotAtPoint($worldName, $worldSettings, $position->getSide(Facing::NORTH, $roadSize));
+                $basePlotInSouth = $this->getBasePlotAtPoint($worldName, $worldSettings, $position->getSide(Facing::SOUTH, $roadSize));
+                if ($basePlotInNorth instanceof BasePlot && $basePlotInSouth instanceof BasePlot) {
+                    $this->getOrLoadPlotsFromBasePlots(
+                        [$basePlotInNorth, $basePlotInSouth],
+                        static function(array $plots) use($onSuccess) : void {
+                            [$plotInNorth, $plotInSouth] = $plots;
+                            if ($plotInNorth instanceof Plot && $plotInSouth instanceof Plot && $plotInNorth->isSame($plotInSouth)) {
+                                $plot = $plotInNorth;
+                            } else {
+                                $plot = false;
+                            }
+                            if ($onSuccess !== null) {
+                                $onSuccess($plot);
+                            }
+                        },
+                        $onError
+                    );
+                    return;
+                }
+                $basePlotInWest = $this->getBasePlotAtPoint($worldName, $worldSettings, $position->getSide(Facing::WEST, $roadSize));
+                $basePlotInEast = $this->getBasePlotAtPoint($worldName, $worldSettings, $position->getSide(Facing::EAST, $roadSize));
+                if ($basePlotInWest instanceof BasePlot && $basePlotInEast instanceof BasePlot) {
+                    $this->getOrLoadPlotsFromBasePlots(
+                        [$basePlotInWest, $basePlotInEast],
+                        static function(array $plots) use($onSuccess) : void {
+                            [$plotInWest, $plotInEast] = $plots;
+                            if ($plotInWest instanceof Plot && $plotInEast instanceof Plot && $plotInWest->isSame($plotInEast)) {
+                                $plot = $plotInWest;
+                            } else {
+                                $plot = false;
+                            }
+                            if ($onSuccess !== null) {
+                                $onSuccess($plot);
+                            }
+                        },
+                        $onError
+                    );
+                    return;
+                }
+                $basePlotInNorthWest = $this->getBasePlotAtPoint($worldName, $worldSettings, $position->add(- $roadSize, 0, - $roadSize));
+                $basePlotInNorthEast = $this->getBasePlotAtPoint($worldName, $worldSettings, $position->add($roadSize, 0, - $roadSize));
+                $basePlotInSouthWest = $this->getBasePlotAtPoint($worldName, $worldSettings, $position->add(- $roadSize, 0, $roadSize));
+                $basePlotInSouthEast = $this->getBasePlotAtPoint($worldName, $worldSettings, $position->add($roadSize, 0, $roadSize));
+                if ($basePlotInNorthWest instanceof BasePlot && $basePlotInNorthEast instanceof BasePlot && $basePlotInSouthWest instanceof BasePlot && $basePlotInSouthEast instanceof BasePlot) {
+                    $this->getOrLoadPlotsFromBasePlots(
+                        [$basePlotInNorthWest, $basePlotInNorthEast, $basePlotInSouthWest, $basePlotInSouthEast],
+                        static function(array $plots) use($onSuccess) : void {
+                            [$plotInNorthWest, $plotInNorthEast, $plotInSouthWest, $plotInSouthEast] = $plots;
+                            if (
+                                $plotInNorthWest instanceof Plot && $plotInNorthEast instanceof Plot && $plotInSouthWest instanceof Plot && $plotInSouthEast instanceof Plot &&
+                                $plotInNorthWest->isSame($plotInNorthEast) && $plotInNorthWest->isSame($plotInSouthWest) && $plotInNorthWest->isSame($plotInSouthEast)
+                            ) {
+                                $plot = $plotInNorthWest;
+                            } else {
+                                $plot = false;
+                            }
+                            if ($onSuccess !== null) {
+                                $onSuccess($plot);
+                            }
+                        },
+                        $onError
+                    );
+                    return;
+                }
+            },
+            $onError
+        );
+        if (!($worldSettings instanceof WorldSettings)) {
+            return $worldSettings === false ? false : null;
+        }
+        $worldName = $world->getFolderName();
+        $basePlot = $this->getBasePlotAtPoint($worldName, $worldSettings, $position);
+        if ($basePlot instanceof BasePlot) {
+            return $this->getOrLoadPlot($world, $basePlot->getX(), $basePlot->getZ(), $onSuccess, $onError);
+        }
+        $roadSize = $worldSettings->getRoadSize();
+        $basePlotInNorth = $this->getBasePlotAtPoint($worldName, $worldSettings, $position->getSide(Facing::NORTH, $roadSize));
+        $basePlotInSouth = $this->getBasePlotAtPoint($worldName, $worldSettings, $position->getSide(Facing::SOUTH, $roadSize));
+        if ($basePlotInNorth instanceof BasePlot && $basePlotInSouth instanceof BasePlot) {
+            $plots = $this->getOrLoadPlotsFromBasePlots(
+                [$basePlotInNorth, $basePlotInSouth],
+                static function(array $plots) use($onSuccess) : void {
+                    [$plotInNorth, $plotInSouth] = $plots;
+                    if ($plotInNorth instanceof Plot && $plotInSouth instanceof Plot && $plotInNorth->isSame($plotInSouth)) {
+                        $plot = $plotInNorth;
+                    } else {
+                        $plot = false;
+                    }
+                    if ($onSuccess !== null) {
+                        $onSuccess($plot);
+                    }
+                },
+                $onError
+            );
+            if (is_array($plots)) {
+                [$plotInNorth, $plotInSouth] = $plots;
+                if ($plotInNorth instanceof Plot && $plotInSouth instanceof Plot && $plotInNorth->isSame($plotInSouth)) {
+                    return $plotInNorth;
+                }
+                return false;
+            }
+            return null;
+        }
+        $basePlotInWest = $this->getBasePlotAtPoint($worldName, $worldSettings, $position->getSide(Facing::WEST, $roadSize));
+        $basePlotInEast = $this->getBasePlotAtPoint($worldName, $worldSettings, $position->getSide(Facing::EAST, $roadSize));
+        if ($basePlotInWest instanceof BasePlot && $basePlotInEast instanceof BasePlot) {
+            $plots = $this->getOrLoadPlotsFromBasePlots(
+                [$basePlotInWest, $basePlotInEast],
+                static function(array $plots) use($onSuccess) : void {
+                    [$plotInWest, $plotInEast] = $plots;
+                    if ($plotInWest instanceof Plot && $plotInEast instanceof Plot && $plotInWest->isSame($plotInEast)) {
+                        $plot = $plotInWest;
+                    } else {
+                        $plot = false;
+                    }
+                    if ($onSuccess !== null) {
+                        $onSuccess($plot);
+                    }
+                },
+                $onError
+            );
+            if (is_array($plots)) {
+                [$plotInWest, $plotInEast] = $plots;
+                if ($plotInWest instanceof Plot && $plotInEast instanceof Plot && $plotInWest->isSame($plotInEast)) {
+                    return $plotInWest;
+                }
+                return false;
+            }
+            return null;
+        }
+        $basePlotInNorthWest = $this->getBasePlotAtPoint($worldName, $worldSettings, $position->add(- $roadSize, 0, - $roadSize));
+        $basePlotInNorthEast = $this->getBasePlotAtPoint($worldName, $worldSettings, $position->add($roadSize, 0, - $roadSize));
+        $basePlotInSouthWest = $this->getBasePlotAtPoint($worldName, $worldSettings, $position->add(- $roadSize, 0, $roadSize));
+        $basePlotInSouthEast = $this->getBasePlotAtPoint($worldName, $worldSettings, $position->add($roadSize, 0, $roadSize));
+        if ($basePlotInNorthWest instanceof BasePlot && $basePlotInNorthEast instanceof BasePlot && $basePlotInSouthWest instanceof BasePlot && $basePlotInSouthEast instanceof BasePlot) {
+            $plots = $this->getOrLoadPlotsFromBasePlots(
+                [$basePlotInNorthWest, $basePlotInNorthEast, $basePlotInSouthWest, $basePlotInSouthEast],
+                static function(array $plots) use($onSuccess) : void {
+                    [$plotInNorthWest, $plotInNorthEast, $plotInSouthWest, $plotInSouthEast] = $plots;
+                    if (
+                        $plotInNorthWest instanceof Plot && $plotInNorthEast instanceof Plot && $plotInSouthWest instanceof Plot && $plotInSouthEast instanceof Plot &&
+                        $plotInNorthWest->isSame($plotInNorthEast) && $plotInNorthWest->isSame($plotInSouthWest) && $plotInNorthWest->isSame($plotInSouthEast)
+                    ) {
+                        $plot = $plotInNorthWest;
+                    } else {
+                        $plot = false;
+                    }
+                    if ($onSuccess !== null) {
+                        $onSuccess($plot);
+                    }
+                },
+                $onError
+            );
+            if (is_array($plots)) {
+                [$plotInNorthWest, $plotInNorthEast, $plotInSouthWest, $plotInSouthEast] = $plots;
+                if (
+                    $plotInNorthWest instanceof Plot && $plotInNorthEast instanceof Plot && $plotInSouthWest instanceof Plot && $plotInSouthEast instanceof Plot &&
+                    $plotInNorthWest->isSame($plotInNorthEast) && $plotInNorthWest->isSame($plotInSouthWest) && $plotInNorthWest->isSame($plotInSouthEast)
+                ) {
+                    return $plotInNorthWest;
+                }
+                return false;
+            }
+            return null;
+        }
+        return false;
+    }
+
+    /**
+     * Get the {@see BasePlot} at the given point in the given world.
+     *
+     * @param string $worldName The name of the world
+     * @param WorldSettings $worldSettings The settings of the world
+     * @param Vector3 $point The point to check represented with a Vector3 object
+     *
+     * Returns the {@see BasePlot} at the given point or false if no base plot exists there.
+     * @return BasePlot|false
+     */
+    public function getBasePlotAtPoint(string $worldName, WorldSettings $worldSettings, Vector3 $point) : BasePlot|false {
+        $roadSize = $worldSettings->getRoadSize();
+        $plotSize = $worldSettings->getPlotSize();
+        $roadPlotSize = $roadSize + $plotSize;
+        $x = $point->getFloorX() - $roadSize;
+        if ($x >= 0) {
+            $X = (int) floor($x / $roadPlotSize);
+            $difX = $x % $roadPlotSize;
+        } else {
+            $X = (int) ceil(($x - $plotSize + 1) / $roadPlotSize);
+            $difX = abs(($x - $plotSize + 1) % $roadPlotSize);
+        }
+        $z = $point->getFloorZ() - $roadSize;
+        if ($z >= 0) {
+            $Z = (int) floor($z / $roadPlotSize);
+            $difZ = $z % $roadPlotSize;
+        } else {
+            $Z = (int) ceil(($z - $plotSize + 1) / $roadPlotSize);
+            $difZ = abs(($z - $plotSize + 1) % $roadPlotSize);
+        }
+        if (($difX > $plotSize - 1) || ($difZ > $plotSize - 1)) {
+            return false;
+        }
+        return new BasePlot($worldName, $worldSettings, $X, $Z);
+    }
+
+    /**
+     * Converts the given array of {@see BasePlot}s to an array of corresponding {@see Plot}s.
+     *
+     * @param array $basePlots The array of {@see BasePlot} objects
+     * @phpstan-param non-empty-array<mixed, BasePlot> $basePlots
+     *
+     * If data about the plots is cached, the $onSuccess function is called immediately, while also letting the method
+     * return the array of plot objects.
+     * If no data about a plot is cached, it needs to be asychronously loaded from the database. Once this is done,
+     * the $onSuccess function is called and the result cached for the next call of this method.
+     *
+     * @param Closure|null $onSuccess The callback function to call if the data about the plots is cached or loaded
+     *                                successfully.
+     * @phpstan-param (Closure(non-empty-array<mixed, Plot|false>):void)|null $onSuccess
+     *
+     * @param Closure|null $onError The callback function to call if something went wrong during the loading of the
+     *                              data from the database.
+     * @phpstan-param (Closure():void)|(Closure(Throwable):void)|null $onError
+     *
+     * @param array $plots The array of already converted {@see Plot} objects. This parameter is for internal use only.
+     * @phpstan-param array<mixed, Plot|false> $plots
+     *
+     * Returns the array of {@see Plot}s, or null if there is no cached data about them, which could synchronously be
+     * gotten.
+     * @return non-empty-array<mixed, Plot|false>|null
+     *
+     * @throws RuntimeException when called outside of main thread.
+     */
+    public function getOrLoadPlotsFromBasePlots(array $basePlots, ?Closure $onSuccess = null, ?Closure $onError = null, array $plots = []) : array|null {
+        foreach($basePlots as $key => $basePlot) {
+            if (isset($plots[$key])) {
+                continue;
+            }
+            $plot = $this->getOrLoadPlot($basePlot->getWorld(), $basePlot->getX(), $basePlot->getZ());
+            if ($plot instanceof Plot || $plot === false) {
+                $plots[$key] = $plot;
+                continue;
+            }
+            $this->getOrLoadPlot(
+                $basePlot->getWorld(), $basePlot->getX(), $basePlot->getZ(),
+                function(Plot|false $plot) use($basePlots, $key, $onSuccess, $onError, $plots) : void {
+                    $plots[$key] = $plot;
+                    $this->getOrLoadPlotsFromBasePlots($basePlots, $onSuccess, $onError, $plots);
+                },
+                $onError
+            );
+            break;
+        }
+        if (count($plots) === count($basePlots)) {
+            if ($onSuccess !== null) {
+                $onSuccess($plots);
+            }
+            return $plots;
+        }
+        return null;
     }
 }
