2412abfc2adf188ca84f378c66426bff28c64464
ColinHDev <colinheidfeld@gmail.com>
2022-08-02 12:47:38 +0200

    PlotClearAsyncTask: Removed "hasPlotOwner" property
    closes #38

--- src/ColinHDev/CPlot/tasks/async/PlotClearAsyncTask.php
+++ src/ColinHDev/CPlot/tasks/async/PlotClearAsyncTask.php
@@ -18,6 +18,7 @@ use pocketmine\world\format\io\FastChunkSerializer;
 use pocketmine\world\format\SubChunk;
 use pocketmine\world\utils\SubChunkExplorer;
 use pocketmine\world\World;
+use const DIRECTORY_SEPARATOR;
 
 class PlotClearAsyncTask extends ChunkModifyingAsyncTask {
     use PlotAreaCalculationTrait;
@@ -25,12 +26,10 @@ class PlotClearAsyncTask extends ChunkModifyingAsyncTask {
     use RoadAreaCalculationTrait;
 
     private string $worldSettings;
-    private string $hasPlotOwner;
 
     public function __construct(Plot $plot) {
         $worldSettings = $plot->getWorldSettings();
         $this->worldSettings = serialize($worldSettings->toArray());
-        $this->hasPlotOwner = serialize($plot->hasPlotOwner());
 
         $chunks = [];
         $this->getChunksFromAreas("plot", $this->calculateBasePlotAreas($worldSettings, $plot), $chunks);
@@ -59,18 +58,9 @@ class PlotClearAsyncTask extends ChunkModifyingAsyncTask {
         /** @phpstan-var array{worldType: string, roadSchematic: string, mergeRoadSchematic: string, plotSchematic: string, roadSize: int, plotSize: int, groundSize: int, roadBlock: string, borderBlock: string, borderBlockOnClaim: string, plotFloorBlock: string, plotFillBlock: string, plotBottomBlock: string} $worldSettingsArray */
         $worldSettingsArray = unserialize($this->worldSettings, ["allowed_classes" => false]);
         $worldSettings = WorldSettings::fromArray($worldSettingsArray);
-        /** @var bool $hasPlotOwner */
-        $hasPlotOwner = unserialize($this->hasPlotOwner, ["allowed_classes" => false]);
 
         $schematicRoad = null;
-        if (!$hasPlotOwner) {
-            if ($worldSettings->getRoadSchematic() !== "default") {
-                $schematicRoad = new Schematic($worldSettings->getRoadSchematic(), "plugin_data" . DIRECTORY_SEPARATOR . "CPlot" . DIRECTORY_SEPARATOR . "schematics" . DIRECTORY_SEPARATOR . $worldSettings->getRoadSchematic() . "." . Schematic::FILE_EXTENSION);
-                if (!$schematicRoad->loadFromFile()) {
-                    $schematicRoad = null;
-                }
-            }
-        } else if ($worldSettings->getMergeRoadSchematic() !== "default") {
+        if ($worldSettings->getMergeRoadSchematic() !== "default") {
             $schematicRoad = new Schematic($worldSettings->getMergeRoadSchematic(), "plugin_data" . DIRECTORY_SEPARATOR . "CPlot" . DIRECTORY_SEPARATOR . "schematics" . DIRECTORY_SEPARATOR . $worldSettings->getMergeRoadSchematic() . "." . Schematic::FILE_EXTENSION);
             if (!$schematicRoad->loadFromFile()) {
                 $schematicRoad = null;
@@ -156,45 +146,24 @@ class PlotClearAsyncTask extends ChunkModifyingAsyncTask {
                             }
                         }
                     } else {
-                        if (!$hasPlotOwner) {
-                            for ($y = $world->getMinY(); $y < $world->getMaxY(); $y++) {
-                                if ($y === $world->getMinY()) {
-                                    $fullBlock = $worldSettings->getPlotBottomBlock()->getFullId();
-                                } else if ($y <= $worldSettings->getGroundSize()) {
-                                    $fullBlock = $worldSettings->getRoadBlock()->getFullId();
-                                } else {
-                                    $fullBlock = 0;
-                                }
-                                $explorer->moveTo($x, $y, $z);
-                                if ($explorer->currentSubChunk instanceof SubChunk) {
-                                    $explorer->currentSubChunk->setFullBlock(
-                                        $xInChunk,
-                                        $y & 0x0f,
-                                        $zInChunk,
-                                        $fullBlock
-                                    );
-                                }
+                        for ($y = $world->getMinY(); $y < $world->getMaxY(); $y++) {
+                            if ($y === $world->getMinY()) {
+                                $fullBlock = $worldSettings->getPlotBottomBlock()->getFullId();
+                            } else if ($y === $worldSettings->getGroundSize()) {
+                                $fullBlock = $worldSettings->getPlotFloorBlock()->getFullId();
+                            } else if ($y < $worldSettings->getGroundSize()) {
+                                $fullBlock = $worldSettings->getPlotFillBlock()->getFullId();
+                            } else {
+                                $fullBlock = 0;
                             }
-                        } else {
-                            for ($y = $world->getMinY(); $y < $world->getMaxY(); $y++) {
-                                if ($y === $world->getMinY()) {
-                                    $fullBlock = $worldSettings->getPlotBottomBlock()->getFullId();
-                                } else if ($y === $worldSettings->getGroundSize()) {
-                                    $fullBlock = $worldSettings->getPlotFloorBlock()->getFullId();
-                                } else if ($y < $worldSettings->getGroundSize()) {
-                                    $fullBlock = $worldSettings->getPlotFillBlock()->getFullId();
-                                } else {
-                                    $fullBlock = 0;
-                                }
-                                $explorer->moveTo($x, $y, $z);
-                                if ($explorer->currentSubChunk instanceof SubChunk) {
-                                    $explorer->currentSubChunk->setFullBlock(
-                                        $xInChunk,
-                                        $y & 0x0f,
-                                        $zInChunk,
-                                        $fullBlock
-                                    );
-                                }
+                            $explorer->moveTo($x, $y, $z);
+                            if ($explorer->currentSubChunk instanceof SubChunk) {
+                                $explorer->currentSubChunk->setFullBlock(
+                                    $xInChunk,
+                                    $y & 0x0f,
+                                    $zInChunk,
+                                    $fullBlock
+                                );
                             }
                         }
                     }
