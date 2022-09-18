75967b1c3d2e8907436948c5d495949811f422be
ColinHDev <colinheidfeld@gmail.com>
2022-09-18 03:03:08 +0200

    added PlotModificationException
    Signed-off-by: ColinHDev <colinheidfeld@gmail.com>

---
+++ src/ColinHDev/CPlot/plots/utils/PlotModificationException.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+namespace ColinHDev\CPlot\plots\utils;
+
+use Exception;
+use Throwable;
+
+class PlotModificationException extends Exception {
+
+    public const EVENT_CANCELLED = 1;
+    public const CHUNK_LOCKED = 2;
+    public const ASYNC_TASK_FAILED = 3;
+    public const DATABASE_ERROR = 4;
+    public const WORLD_NOT_LOADABLE = 5;
+    public const CHUNK_LOCK_CHANGED = 6;
+
+    public function __construct(int $code, string $message = "", ?Throwable $previous = null) {
+        parent::__construct($message, $code, $previous);
+    }
+
+}
