94f6271194a5bb9477e054ef141036ab5291483f
ColinHDev <colinheidfeld@gmail.com>
2022-09-19 19:38:33 +0200

    CPlotAsyncTask: Remove execution time handling

--- src/ColinHDev/CPlot/tasks/async/CPlotAsyncTask.php
+++ src/ColinHDev/CPlot/tasks/async/CPlotAsyncTask.php
@@ -8,37 +8,6 @@ use pocketmine\scheduler\AsyncTask;
 
 abstract class CPlotAsyncTask extends AsyncTask {
 
-    private int $startTime;
-
-    public function __construct() {
-        $this->startTime = (int) (round(microtime(true) * 1000));
-    }
-
-    public function getElapsedTime() : int {
-        return ((int) (round(microtime(true) * 1000))) - $this->startTime;
-    }
-
-    public function getElapsedTimeString() : string {
-        $ms = $this->getElapsedTime();
-        $min = floor($ms / 60000);
-        $ms -= $min * 60000;
-        $s = floor($ms / 1000);
-        $ms -= $s * 1000;
-        $time = "";
-        if ($min > 0) {
-            $time .= $min . "min";
-        }
-        if ($s > 0) {
-            if ($time !== "") $time .= ", ";
-            $time .= $s . "s";
-        }
-        if ($ms > 0) {
-            if ($time !== "") $time .= ", ";
-            $time .= $ms . "ms";
-        }
-        return $time;
-    }
-
     /**
      * @phpstan-param (callable(static): void)|null $onSuccess
      * @phpstan-param (callable(static): void)|null $onError
