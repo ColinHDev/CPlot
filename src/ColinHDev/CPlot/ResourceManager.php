cc695d0133fa9ade019af5a4192ab8395da89196
ColinHDev <colinheidfeld@gmail.com>
2023-03-26 04:49:38 +0200

    removed border and wall files

--- src/ColinHDev/CPlot/ResourceManager.php
+++ src/ColinHDev/CPlot/ResourceManager.php
@@ -10,9 +10,7 @@ use pocketmine\utils\SingletonTrait;
 class ResourceManager {
     use SingletonTrait;
 
-    private Config $bordersConfig;
     private Config $config;
-    private Config $wallsConfig;
 
     public function __construct() {
         if (!is_dir(CPlot::getInstance()->getDataFolder() . "language")) {
@@ -22,9 +20,7 @@ class ResourceManager {
             mkdir(CPlot::getInstance()->getDataFolder() . "schematics");
         }
 
-        CPlot::getInstance()->saveResource("borders.yml");
         CPlot::getInstance()->saveResource("config.yml");
-        CPlot::getInstance()->saveResource("walls.yml");
 
         foreach (CPlot::getInstance()->getResources() as $path => $fileInfo) {
 			if(str_contains($path, "language") and strtolower($fileInfo->getExtension()) === "ini") {
@@ -32,23 +28,11 @@ class ResourceManager {
 			}
         }
 
-        $this->bordersConfig = new Config(CPlot::getInstance()->getDataFolder() . "borders.yml", Config::YAML);
         $this->config = new Config(CPlot::getInstance()->getDataFolder() . "config.yml", Config::YAML);
-        $this->wallsConfig = new Config(CPlot::getInstance()->getDataFolder() . "walls.yml", Config::YAML);
-    }
-
-    public function getBordersConfig() : Config {
-        $this->bordersConfig->reload();
-        return $this->bordersConfig;
     }
 
     public function getConfig() : Config {
         $this->config->reload();
         return $this->config;
-    }
-
-    public function getWallsConfig() : Config {
-        $this->wallsConfig->reload();
-        return $this->wallsConfig;
     }
 }
